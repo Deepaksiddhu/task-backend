9bb9bd9be284b444feb726556f9a83b8fddfd4f6
deepak.siddhi <deepak.siddhi@timesinternet.in>
2025-07-26 22:22:25 +0530

    change

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,156 +1,150 @@
 import bcrypt from "bcryptjs";
-import {db} from "../libs/db.js"
+import { db } from "../libs/db.js";
 import { UserRole } from "../../generated/prisma/index.js";
 import jwt from "jsonwebtoken";
 
-export const register = async (req , res)=>{
-    const {email , password , name} = req.body;
+export const register = async (req, res) => {
+  const { email, password, name, role } = req.body;
 
-    try {
-        const existingUser = await db.user.findUnique({
-            where:{
-                email
-            }
-        })
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-        if(existingUser){
-            return res.status(400).json({
-                error:"User already exists"
-            })
-        }
-        
-
-        const hashedPassword = await bcrypt.hash(password , 10);
-
-        const newUser = await db.user.create({
-            data:{
-                email,
-                password:hashedPassword,
-                name,
-                role,
-            }
-        })
-
-        const token = jwt.sign({id:newUser.id} , process.env.JWT_SECRET , {
-            expiresIn:"7d"
-        })
-
-        res.cookie("jwt" , token , {
-            httpOnly:true,
-            sameSite:"strict",
-            secure:process.env.NODE_ENV !== "development",
-            maxAge:1000 * 60 * 60 * 24 * 7 // 7 days
-        })
-
-        res.status(201).json({
-            success:true,
-            message:"User created successfully",
-            user:{
-                id:newUser.id,
-                email:newUser.email,
-                name:newUser.name,
-                role:newUser.role,
-                image:newUser.image
-            }
-        })
-
-    } catch (error) {
-            console.error("Error creating user:", error);
-            res.status(500).json({
-                error:"Error creating user"
-            })
+    if (existingUser) {
+      return res.status(400).json({
+        error: "User already exists",
+      });
     }
-}
 
-export const login = async (req , res)=>{
-    const {email , password} = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    try {
-        const user = await db.user.findUnique({
-            where:{
-                email
-            }
-        })
+    const newUser = await db.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        name,
+        role,
+      },
+    });
 
-        if(!user){
-            return res.status(401).json({
-                error:"User not found"
-            })
-        }
+    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
-        const isMatch = await bcrypt.compare(password , user.password);
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    });
 
-        if(!isMatch){
-            return res.status(401).json({
-                error:"Invalid credentials"
-            })
-        }
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        id: newUser.id,
+        email: newUser.email,
+        name: newUser.name,
+        role: newUser.role,
+        image: newUser.image,
+      },
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({
+      error: "Error creating user",
+    });
+  }
+};
 
-        const token = jwt.sign({id:user.id} , process.env.JWT_SECRET,{
-            expiresIn:"7d"
-        })
+export const login = async (req, res) => {
+  const { email, password } = req.body;
 
-        res.cookie("jwt" , token , {
-            httpOnly:true,
-            sameSite:"strict",
-            secure:process.env.NODE_ENV !== "development",
-            maxAge:1000 * 60 * 60 * 24 * 7 // 7 days
-        })
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
 
-        res.status(200).json({
-            success:true,
-            message:"User Logged in successfully",
-            user:{
-                id:user.id,
-                email:user.email,
-                name:user.name,
-                role:user.role,
-                image:user.image
-            }
-        })
-
-        
-    } catch (error) {
-        console.error("Error creating user:", error);
-        res.status(500).json({
-            error:"Error logging in user"
-        })
+    if (!user) {
+      return res.status(401).json({
+        error: "User not found",
+      });
     }
-}
 
+    const isMatch = await bcrypt.compare(password, user.password);
 
-export const logout = async (req , res)=>{
-    try {
-        res.clearCookie("jwt" , {
-            httpOnly:true,
-            sameSite:"strict",
-            secure:process.env.NODE_ENV !== "development",
-        })
-
-        res.status(200).json({
-            success:true,
-            message:"User logged out successfully"
-        })
-    } catch (error) {
-        console.error("Error logging out user:", error);
-        res.status(500).json({
-            error:"Error logging out user"
-        })
+    if (!isMatch) {
+      return res.status(401).json({
+        error: "Invalid credentials",
+      });
     }
-}
 
-export const check = async (req , res)=>{
-    try {
-        res.status(200).json({
-            success:true,
-            message:"User authenticated successfully",
-            user:req.user
-        });
-    } catch (error) {
-        console.error("Error checking user:", error);
-        res.status(500).json({
-            error:"Error checking user"
-        })
-    }
-}
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
 
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User Logged in successfully",
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role,
+        image: user.image,
+      },
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({
+      error: "Error logging in user",
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: process.env.NODE_ENV !== "development",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "User logged out successfully",
+    });
+  } catch (error) {
+    console.error("Error logging out user:", error);
+    res.status(500).json({
+      error: "Error logging out user",
+    });
+  }
+};
+
+export const check = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: "User authenticated successfully",
+      user: req.user,
+    });
+  } catch (error) {
+    console.error("Error checking user:", error);
+    res.status(500).json({
+      error: "Error checking user",
+    });
+  }
+};
